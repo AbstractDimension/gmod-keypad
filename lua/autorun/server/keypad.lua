bc4169363041a0142b1544a8bbcb6ab4ebc38766
Redox <mail@redox.to>
2022-08-14 00:36:54 +0200

    Move convars to server

---
+++ lua/autorun/server/keypad.lua
@@ -0,0 +1,2 @@
+CreateConVar( "keypad_min_granted_hold_lenght", 5, { FCVAR_ARCHIVE }, "Minimum time a keypad will stay open when its opened.", 0 )
+CreateConVar( "keypad_max_granted_initial_lenght", 3, { FCVAR_ARCHIVE }, "Maximum time a keypad will wait to open after its granted.", 0 )
