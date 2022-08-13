5b94e3a2ad4ba23b47ed7c77059b80eaef0ba0f6
Redox <mail@redox.to>
2022-08-14 00:06:23 +0200

    Run formatter, remove unused and fix deprecated

--- lua/entities/keypad/cl_maths.lua
+++ lua/entities/keypad/cl_maths.lua
@@ -1,56 +1,41 @@
 -- Avoiding the clutter
-
 function ENT:CalculateCursorPos()
-	local ply = LocalPlayer()
+    local ply = LocalPlayer()
+    if not IsValid( ply ) then return 0, 0 end
 
-	if not IsValid(ply) then
-		return 0, 0
-	end
+    local tr = util.TraceLine( {
+        start = ply:EyePos(),
+        endpos = ply:EyePos() + ply:GetAimVector() * 65,
+        filter = ply
+    } )
 
-	local tr = util.TraceLine({
-		start = ply:EyePos(),
-		endpos = ply:EyePos() + ply:GetAimVector() * 65,
-		filter = ply
-	})
+    if tr.Entity ~= self then return 0, 0 end
+    local scale = self.Scale
+    local pos, ang = self:CalculateRenderPos(), self:CalculateRenderAng()
+    local normal = self:GetForward()
+    local intersection = util.IntersectRayWithPlane( ply:EyePos(), ply:GetAimVector(), pos, normal )
+    if not intersection then return 0, 0 end
+    local diff = pos - intersection
+    local x = diff:Dot( -ang:Forward() ) / scale
+    local y = diff:Dot( -ang:Right() ) / scale
 
-	if tr.Entity ~= self then
-		return 0, 0
-	end
-
-	local scale = self.Scale
-
-	local pos, ang = self:CalculateRenderPos(), self:CalculateRenderAng()
-	local normal = self:GetForward()
-	
-	local intersection = util.IntersectRayWithPlane(ply:EyePos(), ply:GetAimVector(), pos, normal)
-	
-	if not intersection then
-		return 0, 0
-	end
-
-	local diff = pos - intersection
-
-	local x = diff:Dot(-ang:Forward()) / scale
-	local y = diff:Dot(-ang:Right()) / scale
-
-	return x, y
+    return x, y
 end
 
 function ENT:CalculateRenderPos()
-	local pos = self:GetPos()
-		pos:Add(self:GetForward() * self.Maxs.x) -- Translate to front
-		pos:Add(self:GetRight() * self.Maxs.y) -- Translate to left
-		pos:Add(self:GetUp() * self.Maxs.z) -- Translate to top
+    local pos = self:GetPos()
+    pos:Add( self:GetForward() * self.Maxs.x ) -- Translate to front
+    pos:Add( self:GetRight() * self.Maxs.y ) -- Translate to left
+    pos:Add( self:GetUp() * self.Maxs.z ) -- Translate to top
+    pos:Add( self:GetForward() * 0.15 ) -- Pop out of front to stop culling
 
-		pos:Add(self:GetForward() * 0.15) -- Pop out of front to stop culling
-
-	return pos
+    return pos
 end
 
 function ENT:CalculateRenderAng()
-	local ang = self:GetAngles()
-		ang:RotateAroundAxis(ang:Right(), -90)
-		ang:RotateAroundAxis(ang:Up(), 90)	
+    local ang = self:GetAngles()
+    ang:RotateAroundAxis( ang:Right(), -90 )
+    ang:RotateAroundAxis( ang:Up(), 90 )
 
-	return ang
+    return ang
 end
--- lua/weapons/gmod_tool/stools/keypad_willox_wire.lua
+++ lua/weapons/gmod_tool/stools/keypad_willox_wire.lua
@@ -1,209 +1,173 @@
-if (SERVER) then
-	-- CreateConVar('sbox_maxkeypads', 10) -- Handled by keypad_willox.lua
-end
-
 TOOL.Category = "Construction"
 TOOL.Name = "Keypad - Wire"
 TOOL.Command = nil
-
-TOOL.ClientConVar['weld'] = '1'
-TOOL.ClientConVar['freeze'] = '1'
-
-TOOL.ClientConVar['password'] = '1234'
-TOOL.ClientConVar['secure'] = '0'
-
-TOOL.ClientConVar['repeats_granted'] = '0'
-TOOL.ClientConVar['repeats_denied'] = '0'
-
-TOOL.ClientConVar['length_granted'] = '0.1'
-TOOL.ClientConVar['length_denied'] = '0.1'
-
-TOOL.ClientConVar['delay_granted'] = '0'
-TOOL.ClientConVar['delay_denied'] = '0'
-
-TOOL.ClientConVar['init_delay_granted'] = '0'
-TOOL.ClientConVar['init_delay_denied'] = '0'
-
-TOOL.ClientConVar['output_on'] = '1'
-TOOL.ClientConVar['output_off'] = '0'
+TOOL.ClientConVar["weld"] = "1"
+TOOL.ClientConVar["freeze"] = "1"
+TOOL.ClientConVar["password"] = "1234"
+TOOL.ClientConVar["secure"] = "0"
+TOOL.ClientConVar["repeats_granted"] = "0"
+TOOL.ClientConVar["repeats_denied"] = "0"
+TOOL.ClientConVar["length_granted"] = "0.1"
+TOOL.ClientConVar["length_denied"] = "0.1"
+TOOL.ClientConVar["delay_granted"] = "0"
+TOOL.ClientConVar["delay_denied"] = "0"
+TOOL.ClientConVar["init_delay_granted"] = "0"
+TOOL.ClientConVar["init_delay_denied"] = "0"
+TOOL.ClientConVar["output_on"] = "1"
+TOOL.ClientConVar["output_off"] = "0"
 
 -- cleanup.Register("keypads") -- Handled by keypad_willox.lua
-
 if CLIENT then
-	language.Add("tool.keypad_willox_wire.name", "Keypad - Wire")
-	language.Add("tool.keypad_willox_wire.0", "Left Click: Create, Right Click: Update")
-	language.Add("tool.keypad_willox_wire.desc", "Creates Keypads for secure access")
-
-	--[[
+    language.Add( "tool.keypad_willox_wire.name", "Keypad - Wire" )
+    language.Add( "tool.keypad_willox_wire.0", "Left Click: Create, Right Click: Update" )
+    language.Add( "tool.keypad_willox_wire.desc", "Creates Keypads for secure access" )
+    --[[
         language.Add("Undone_Keypad", "Undone Keypad")
     	language.Add("Cleanup_keypads", "Keypads")
     	language.Add("Cleaned_keypads", "Cleaned up all Keypads")
 
-    	language.Add("SBoxLimit_keypads", "You've hit the Keypad limit!")
-    ]] -- Handled by keypad_willox.lua
+    	language.Add("SBoxLimit_keypads", "You"ve hit the Keypad limit!")
+    ]]
+    -- Handled by keypad_willox.lua
 end
 
-function TOOL:SetupKeypad(ent, pass)
-	local data = {
-		Password = pass,
+function TOOL:SetupKeypad( ent, pass )
+    local data = {
+        Password = pass,
+        RepeatsGranted = self:GetClientNumber( "repeats_granted" ),
+        RepeatsDenied = self:GetClientNumber( "repeats_denied" ),
+        LengthGranted = self:GetClientNumber( "length_granted" ),
+        LengthDenied = self:GetClientNumber( "length_denied" ),
+        DelayGranted = self:GetClientNumber( "delay_granted" ),
+        DelayDenied = self:GetClientNumber( "delay_denied" ),
+        InitDelayGranted = self:GetClientNumber( "init_delay_granted" ),
+        InitDelayDenied = self:GetClientNumber( "init_delay_denied" ),
+        OutputOn = self:GetClientNumber( "output_on" ),
+        OutputOff = self:GetClientNumber( "output_off" ),
+        Secure = util.tobool( self:GetClientNumber( "secure" ) )
+    }
 
-		RepeatsGranted = self:GetClientNumber("repeats_granted"),
-		RepeatsDenied = self:GetClientNumber("repeats_denied"),
-
-		LengthGranted = self:GetClientNumber("length_granted"),
-		LengthDenied = self:GetClientNumber("length_denied"),
-
-		DelayGranted = self:GetClientNumber("delay_granted"),
-		DelayDenied = self:GetClientNumber("delay_denied"),
-
-		InitDelayGranted = self:GetClientNumber("init_delay_granted"),
-		InitDelayDenied = self:GetClientNumber("init_delay_denied"),
-
-		OutputOn = self:GetClientNumber("output_on"),
-		OutputOff = self:GetClientNumber("output_off"),
-
-		Secure = util.tobool(self:GetClientNumber("secure"))
-	}
-
-	ent:SetData(data)
+    ent:SetData( data )
 end
 
-function TOOL:RightClick(tr)
+function TOOL:RightClick( tr )
     if not WireLib then return false end
-	if not IsValid(tr.Entity) or not tr.Entity:GetClass():lower() == "keypad_wire" then return false end
+    if not IsValid( tr.Entity ) or not tr.Entity:GetClass():lower() == "keypad_wire" then return false end
+    if CLIENT then return true end
+    local ply = self:GetOwner()
+    local password = tonumber( ply:GetInfo( "keypad_willox_wire_password" ) )
+    local trace_ent = tr.Entity
 
-	if CLIENT then return true end
+    if password == nil or string.len( tostring( password ) ) > 4 or string.find( tostring( password ), "0" ) then
+        ply:PrintMessage( 3, "Invalid password!" )
 
-	local ply = self:GetOwner()
-	local password = tonumber(ply:GetInfo("keypad_willox_wire_password"))
+        return false
+    end
 
-	local spawn_pos = tr.HitPos
-	local trace_ent = tr.Entity
+    self:SetupKeypad( trace_ent, password )
 
-	if password == nil or (string.len(tostring(password)) > 4) or (string.find(tostring(password), "0")) then
-		ply:PrintMessage(3, "Invalid password!")
-		return false
-	end
-
-	self:SetupKeypad(trace_ent, password)
-	return true
+    return true
 end
 
-function TOOL:LeftClick(tr)
+function TOOL:LeftClick( tr )
     if not WireLib then return false end
-	if IsValid(tr.Entity) and tr.Entity:GetClass() == "player" then return false end
+    if IsValid( tr.Entity ) and tr.Entity:GetClass() == "player" then return false end
+    if CLIENT then return true end
+    local ply = self:GetOwner()
+    local password = self:GetClientNumber( "password" )
+    local spawn_pos = tr.HitPos + tr.HitNormal
+    local trace_ent = tr.Entity
 
-	if CLIENT then return true end
+    if password == nil or string.len( tostring( password ) ) > 4 or string.find( tostring( password ), "0" ) then
+        ply:PrintMessage( 3, "Invalid password!" )
 
-	local ply = self:GetOwner()
-	local password = self:GetClientNumber("password")
+        return false
+    end
 
-	local spawn_pos = tr.HitPos + tr.HitNormal
-	local trace_ent = tr.Entity
+    if not self:GetWeapon():CheckLimit( "keypads" ) then return false end
+    local ent = ents.Create( "keypad_wire" )
+    ent:SetPos( spawn_pos )
+    ent:SetAngles( tr.HitNormal:Angle() )
+    ent:Spawn()
+    ent:SetPlayer( ply )
+    local freeze = tobool( self:GetClientNumber( "freeze" ) )
+    local weld = tobool( self:GetClientNumber( "weld" ) )
 
-	if password == nil or (string.len(tostring(password)) > 4) or (string.find(tostring(password), "0")) then
-		ply:PrintMessage(3, "Invalid password!")
-		return false
-	end
+    if freeze or weld then
+        local phys = ent:GetPhysicsObject()
 
-	if not self:GetWeapon():CheckLimit("keypads") then return false end
+        if IsValid( phys ) then
+            phys:EnableMotion( false )
+        end
+    end
 
-	local ent = ents.Create("keypad_wire")
-	ent:SetPos(spawn_pos)
-	ent:SetAngles(tr.HitNormal:Angle())
-	ent:Spawn()
+    if weld then
+        constraint.Weld( ent, trace_ent, 0, 0, 0, true, false )
+    end
 
-	ent:SetPlayer(ply)
+    self:SetupKeypad( ent, password )
+    undo.Create( "Keypad" )
+    undo.AddEntity( ent )
+    undo.SetPlayer( ply )
+    undo.Finish()
+    ply:AddCount( "keypads", ent )
+    ply:AddCleanup( "keypads", ent )
 
-	local freeze = util.tobool(self:GetClientNumber("freeze"))
-	local weld = util.tobool(self:GetClientNumber("weld"))
-
-	if freeze or weld then
-		local phys = ent:GetPhysicsObject() 
-
-		if IsValid(phys) then
-			phys:EnableMotion(false)
-		end
-	end
-
-	if weld then
-		local weld = constraint.Weld(ent, trace_ent, 0, 0, 0, true, false)
-	end
-
-	self:SetupKeypad(ent, password)
-
-	undo.Create("Keypad")
-		undo.AddEntity(ent)
-		undo.SetPlayer(ply)
-	undo.Finish()
-
-	ply:AddCount("keypads", ent)
-	ply:AddCleanup("keypads", ent)
-
-	return true
+    return true
 end
-
 
 if CLIENT then
-	local function ResetSettings(ply)
-		ply:ConCommand("keypad_willox_wire_repeats_granted 0")
-		ply:ConCommand("keypad_willox_wire_repeats_denied 0")
-		ply:ConCommand("keypad_willox_wire_length_granted 0.1")
-		ply:ConCommand("keypad_willox_wire_length_denied 0.1")
-		ply:ConCommand("keypad_willox_wire_delay_granted 0")
-		ply:ConCommand("keypad_willox_wire_delay_denied 0")
-		ply:ConCommand("keypad_willox_wire_init_delay_granted 0")
-		ply:ConCommand("keypad_willox_wire_init_delay_denied 0")
-        ply:ConCommand("keypad_willox_wire_output_on 1")
-        ply:ConCommand("keypad_willox_wire_output_off 0")
-	end
+    local function ResetSettings( ply )
+        ply:ConCommand( "keypad_willox_wire_repeats_granted 0" )
+        ply:ConCommand( "keypad_willox_wire_repeats_denied 0" )
+        ply:ConCommand( "keypad_willox_wire_length_granted 0.1" )
+        ply:ConCommand( "keypad_willox_wire_length_denied 0.1" )
+        ply:ConCommand( "keypad_willox_wire_delay_granted 0" )
+        ply:ConCommand( "keypad_willox_wire_delay_denied 0" )
+        ply:ConCommand( "keypad_willox_wire_init_delay_granted 0" )
+        ply:ConCommand( "keypad_willox_wire_init_delay_denied 0" )
+        ply:ConCommand( "keypad_willox_wire_output_on 1" )
+        ply:ConCommand( "keypad_willox_wire_output_off 0" )
+    end
 
-	concommand.Add("keypad_willox_wire_reset", ResetSettings)
+    concommand.Add( "keypad_willox_wire_reset", ResetSettings )
 
-	function TOOL.BuildCPanel(CPanel)
-        if not WireLib then 
-            CPanel:Help("This tool requires Wiremod to function")
-            CPanel:Help("http://wiremod.com/")
-            CPanel:Help("Workshop Addon #160250458")
+    function TOOL.BuildCPanel( CPanel )
+        if not WireLib then
+            CPanel:Help( "This tool requires Wiremod to function" )
+            CPanel:Help( "http://wiremod.com/" )
+            CPanel:Help( "Workshop Addon #160250458" )
         else
-    		local r, l = CPanel:TextEntry("Access Password", "keypad_willox_wire_password")
-    		r:SetTall(22)
+            local r, l = CPanel:TextEntry( "Access Password", "keypad_willox_wire_password" )
+            r:SetTall( 22 )
+            CPanel:ControlHelp( "Max Length: 4\nAllowed Digits: 1-9" )
+            CPanel:CheckBox( "Secure Mode", "keypad_willox_wire_secure" )
+            CPanel:CheckBox( "Weld", "keypad_willox_wire_weld" )
+            CPanel:CheckBox( "Freeze", "keypad_willox_wire_freeze" )
+            CPanel:NumSlider( "Output On:", "keypad_willox_wire_output_on", -10, 10, 0 )
+            CPanel:NumSlider( "Output Off:", "keypad_willox_wire_output_off", -10, 10, 0 )
 
-    		CPanel:ControlHelp("Max Length: 4\nAllowed Digits: 1-9")
+            local granted = vgui.Create( "DForm" )
+            granted:SetName( "Access Granted Settings" )
+            granted:NumSlider( "Hold Length", "keypad_willox_wire_length_granted", 0.1, 10, 2 )
+            granted:NumSlider( "Initial Delay", "keypad_willox_wire_init_delay_granted", 0, 10, 2 )
+            granted:NumSlider( "Multiple Press Delay", "keypad_willox_wire_delay_granted", 0, 10, 2 )
+            granted:NumSlider( "Additional Repeats", "keypad_willox_wire_repeats_granted", 0, 5, 0 )
+            CPanel:AddItem( granted )
 
-    		CPanel:CheckBox("Secure Mode", "keypad_willox_wire_secure")
-    		CPanel:CheckBox("Weld", "keypad_willox_wire_weld")
-    		CPanel:CheckBox("Freeze", "keypad_willox_wire_freeze")
+            local denied = vgui.Create( "DForm" )
+            denied:SetName( "Access Denied Settings" )
+            denied:NumSlider( "Hold Length", "keypad_willox_wire_length_denied", 0.1, 10, 2 )
+            denied:NumSlider( "Initial Delay", "keypad_willox_wire_init_delay_denied", 0, 10, 2 )
+            denied:NumSlider( "Multiple Press Delay", "keypad_willox_wire_delay_denied", 0, 10, 2 )
+            denied:NumSlider( "Additional Repeats", "keypad_willox_wire_repeats_denied", 0, 5, 0 )
+            CPanel:AddItem( denied )
+            CPanel:Button( "Default Settings", "keypad_willox_wire_reset" )
+            CPanel:Help( "" )
 
-            CPanel:NumSlider("Output On:", "keypad_willox_wire_output_on", -10, 10, 0)
-            CPanel:NumSlider("Output Off:", "keypad_willox_wire_output_off", -10, 10, 0)
-
-    		local granted = vgui.Create("DForm")
-    			granted:SetName("Access Granted Settings")
-
-	    		granted:NumSlider("Hold Length", "keypad_willox_wire_length_granted", 0.1, 10, 2)
-	    		granted:NumSlider("Initial Delay", "keypad_willox_wire_init_delay_granted", 0, 10, 2)
-	    		granted:NumSlider("Multiple Press Delay", "keypad_willox_wire_delay_granted", 0, 10, 2)
-	    		granted:NumSlider("Additional Repeats", "keypad_willox_wire_repeats_granted", 0, 5, 0)
-    		CPanel:AddItem(granted)
-    		
-    		local denied = vgui.Create("DForm")
-    			denied:SetName("Access Denied Settings")
-
-	    		denied:NumSlider("Hold Length", "keypad_willox_wire_length_denied", 0.1, 10, 2)
-	    		denied:NumSlider("Initial Delay", "keypad_willox_wire_init_delay_denied", 0, 10, 2)
-	    		denied:NumSlider("Multiple Press Delay", "keypad_willox_wire_delay_denied", 0, 10, 2)
-	    		denied:NumSlider("Additional Repeats", "keypad_willox_wire_repeats_denied", 0, 5, 0)
-    		CPanel:AddItem(denied)
-
-			CPanel:Button("Default Settings", "keypad_willox_wire_reset")
-
-			CPanel:Help("")
-
-			local faq = CPanel:Help("Information")
-				faq:SetFont("GModWorldtip")
-
-			CPanel:Help("You can enter your password with your numpad when numlock is enabled!")
+            local faq = CPanel:Help( "Information" )
+            faq:SetFont( "GModWorldtip" )
+            CPanel:Help( "You can enter your password with your numpad when numlock is enabled!" )
         end
-	end
+    end
 end
