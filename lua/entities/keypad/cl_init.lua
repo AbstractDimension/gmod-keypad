fb60c64a00dccb5c53a30ffa2916658f60394037
Redox <mail@redox.to>
2023-04-26 22:17:51 +0200

    Improve performance

--- lua/entities/keypad/cl_init.lua
+++ lua/entities/keypad/cl_init.lua
@@ -2,21 +2,29 @@ include( "sh_init.lua" )
 include( "cl_maths.lua" )
 include( "cl_panel.lua" )
 
+local render_SetMaterial = render.SetMaterial
+local render_DrawBox = render.DrawBox
+local cam_Start3D2D = cam.Start3D2D
+local color_white = color_white
+local cam_End3D2D = cam.End3D2D
+
 local mat = CreateMaterial( "willox_keypad_material", "VertexLitGeneric", {
     ["$basetexture"] = "white",
     ["$color"] = "{ 36 36 36 }",
 } )
 
 function ENT:Draw()
-    render.SetMaterial( mat )
-    render.DrawBox( self:GetPos(), self:GetAngles(), self.Mins, self.Maxs, color_white, true )
+    local entTable = self:GetTable()
+
+    render_SetMaterial( mat )
+    render_DrawBox( self:GetPos(), self:GetAngles(), entTable.Mins, entTable.Maxs, color_white, true )
     local pos, ang = self:CalculateRenderPos(), self:CalculateRenderAng()
-    local w, h = self.Width2D, self.Height2D
+    local w, h = entTable.Width2D, entTable.Height2D
     local x, y = self:CalculateCursorPos()
 
-    cam.Start3D2D( pos, ang, self.Scale )
+    cam_Start3D2D( pos, ang, self.Scale )
     self:Paint( w, h, x, y )
-    cam.End3D2D()
+    cam_End3D2D()
 end
 
 function ENT:SendCommand( command, data )
